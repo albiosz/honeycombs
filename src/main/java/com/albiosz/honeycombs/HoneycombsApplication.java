f80d8265f5c003ea17bc964f8b5812ab10374496
albiosz <szulcalbert@gmail.com>
2025-01-27 11:38:14 +0100

    init - Spring Initializr

---
+++ src/main/java/com/albiosz/honeycombs/HoneycombsApplication.java
@@ -0,0 +1,13 @@
+package com.albiosz.honeycombs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneycombsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneycombsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albiosz/honeycombs/HoneycombsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albiosz.honeycombs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneycombsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
