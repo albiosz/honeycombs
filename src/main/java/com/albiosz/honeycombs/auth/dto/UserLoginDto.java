4d617f0c785b3b6b5466de922216e314c9b77d75
albiosz <szulcalbert@gmail.com>
2025-02-11 16:27:39 +0100

    add junit and mockito for further testing

--- src/main/java/com/albiosz/honeycombs/auth/dto/UserLoginDto.java
+++ src/main/java/com/albiosz/honeycombs/auth/dto/UserLoginDto.java
@@ -1,10 +1,12 @@
 package com.albiosz.honeycombs.auth.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class UserLoginDto {
 	private String username;
 	private String password;
---
+++ src/test/java/com/albiosz/honeycombs/auth/AuthServiceTest.java
@@ -0,0 +1,42 @@
+package com.albiosz.honeycombs.auth;
+
+import com.albiosz.honeycombs.user.User;
+import com.albiosz.honeycombs.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import com.albiosz.honeycombs.auth.dto.UserLoginDto;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AuthServiceTest {
+
+	private UserRepository userRepositoryMock;
+	private AuthenticationManager authenticationManager;
+
+	@BeforeEach
+	void setUp() {
+		User user = new User("user", "password", "nickname", true);
+
+		userRepositoryMock = mock(UserRepository.class);
+		when(userRepositoryMock.findByUsername("user")).thenReturn(Optional.of(user));
+
+		authenticationManager = mock(AuthenticationManager.class);
+		when(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken("user", "password"))).thenReturn(null);
+	}
+
+	@Test
+	void testLogin() {
+		AuthService authService = new AuthService(userRepositoryMock, null, authenticationManager, null);
+		User returnedUser = authService.login(new UserLoginDto("user", "password"));
+
+		assert(returnedUser.getUsername().equals("user"));
+		assert(returnedUser.getPassword().equals("password"));
+
+	}
+}
