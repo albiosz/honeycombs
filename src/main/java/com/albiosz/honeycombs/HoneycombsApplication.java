15040dbc0562082472f822efe16627dabbaf2c0e
albiosz <szulcalbert@gmail.com>
2025-03-11 11:29:22 +0100

    add basic logging of user actions

--- src/main/java/com/albiosz/honeycombs/HoneycombsApplication.java
+++ src/main/java/com/albiosz/honeycombs/HoneycombsApplication.java
@@ -1,5 +1,7 @@
 package com.albiosz.honeycombs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,8 @@ import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class HoneycombsApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger("com.example.useractivity");
 
 	public static void main(String[] args) {
 		DBCheck dbCheck = new DBCheck();
@@ -18,6 +22,7 @@ public class HoneycombsApplication {
 	@Bean
 	@Profile("!test")
 	CommandLineRunner setUpDatabase(DBConfig dbConfig) {
+		logger.info("LOADING SEEDS TO DB");
 		return args -> dbConfig.initDB();
 	}
 }
---
+++ src/main/java/com/albiosz/honeycombs/config/logging/WebConfig.java
@@ -0,0 +1,23 @@
+package com.albiosz.honeycombs.config.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private RequestLoggingInterceptor requestLoggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestLoggingInterceptor)
+            .addPathPatterns("/api/auth/**")
+            .addPathPatterns("/api/user/**")
+            .addPathPatterns("/api/game/**");
+    }
+}
